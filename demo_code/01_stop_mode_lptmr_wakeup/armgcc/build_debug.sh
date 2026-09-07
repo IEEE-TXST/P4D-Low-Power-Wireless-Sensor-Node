748df97fb96dd0e5aa1fe4b40a32223362865420
Ashok Paudel <ashokpaudelapril@gmail.com>
2026-09-07 01:34:57 -0500

    Remove leftover armgcc build scaffolding and stale .srec from demo_code

--- demo_code/01_stop_mode_lptmr_wakeup/armgcc/build_debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../SDK_2_2_0_FRDM-KL26Z/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Debug  .
-make -j4
